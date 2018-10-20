96a1afe9f3370d41081e06f1de24b2f9d254152a
Yashar <ya.atajan@gmail.com>
2018-10-20 12:40:13 -0400

    Update app/src/main/java/com/sensationmeter/yaxar/dominion/HomeActivity.kt

--- app/src/main/java/com/sensationmeter/yaxar/dominion/HomeActivity.kt
+++ app/src/main/java/com/sensationmeter/yaxar/dominion/HomeActivity.kt
@@ -1,12 +1,35 @@
 package com.sensationmeter.yaxar.dominion
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Initializations
+        setButtonColor()
+
+        // Button listeners
+        homeBt.setOnClickListener {
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        accountBt.setOnClickListener {
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        savingsBt.setOnClickListener {
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setButtonColor() {
+        homeBt.setTextColor(0xFF3B74CD.toInt())
     }
 }
